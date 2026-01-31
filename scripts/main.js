fc1ce44f967cc6a5efbadfccf7d2e0f666d2ec92
N3TTI3 <kboyd076@uottawa.ca>
2026-01-31 12:53:01 -0500

    - wrong price default values

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 let clientVeg = false;
 let clientGF = true;
 let organicState = "all";
-let clientFrom = 0;
-let clientTo = 0;
+let clientFrom = 0.00;
+let clientTo = 100.00;
 
 // This function is called when any of the tab is clicked
 // It is adapted from https://www.w3schools.com/howto/howto_js_tabs.asp
